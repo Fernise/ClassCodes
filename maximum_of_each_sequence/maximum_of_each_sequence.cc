e156c8a8e7f51b005003e3793123de970cee6126
Fernise <alu0101506371@ull.edu.es>
2021-12-01 22:55:11 +0000

    Prueba

--- maximum_of_each_sequence/maximum_of_each_sequence.cc
+++ maximum_of_each_sequence/maximum_of_each_sequence.cc
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <vector>
+
+int LookingForMax(std::vector<int>& numbers_serie, const int kSerieLength) {
+  int max_number = numbers_serie[0];
+  for (int i = 0; i < kSerieLength; ++i) {
+    for (int j = 0; j < kSerieLength; ++j) {
+      if ((numbers_serie[i] >= numbers_serie[j]) && (numbers_serie[i] >= max_number)) {
+        max_number = numbers_serie[i];
+      }
+    }
+  }
+
+  return max_number;
+}
 
 int main() {
-  std::cout << "Este programa lee una secuencia de números y muestra por pantalla el mayor de esa secuencia" << std::endl;
-  
+  // std::cout << "Este programa lee una secuencia de números y muestra por pantalla el mayor de esa secuencia" << std::endl;
+  // std::cout << "Introduzca la longitud de la secuencia de números: ";
+  int serie_length = 0;
+  std::vector<int> numbers_serie;
+  // std::cout << "Introduzca la secuencia de números: ";
+  int contador = 0;
+  while (contador != 3) {
+    std::cin >> serie_length;
+    numbers_serie.reserve(serie_length);
+    for (int i = 0; i < serie_length; ++i) {
+      std::cin >> numbers_serie[i];
+    }
+    std::cout << LookingForMax(numbers_serie, serie_length) << std::endl;
+    ++contador;
+  }
 
   return 0;
 }
